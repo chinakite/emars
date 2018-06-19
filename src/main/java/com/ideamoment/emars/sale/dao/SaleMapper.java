6cafc955842c855c516ae571732c9d45872c762a
chinakite <chinakite.zhang@gmail.com>
2018-06-20 07:09:40 +0800

    list sale contract

--- src/main/java/com/ideamoment/emars/sale/dao/SaleMapper.java
+++ src/main/java/com/ideamoment/emars/sale/dao/SaleMapper.java
@@ -2,9 +2,7 @@ package com.ideamoment.emars.sale.dao;
 
 import com.ideamoment.emars.model.Sale;
 import com.ideamoment.emars.model.SaleContractQueryVo;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +12,22 @@ import java.util.List;
  */
 @Mapper
 public interface SaleMapper {
+    @Select("select * from t_sale where c_id = #{id}")
+    @Results(id = "saleMap", value ={
+            @Result(property = "id", column = "c_id", id = true),
+            @Result(property = "code", column = "C_CODE"),
+            @Result(property = "type", column = "C_TYPE"),
+            @Result(property = "owner", column = "C_OWNER"),
+            @Result(property = "totalSection", column = "C_TOTAL_SECTION"),
+            @Result(property = "totalPrice", column = "C_TOTAL_PRICE"),
+            @Result(property = "signDate", column = "C_SIGNDATE"),
+            @Result(property = "creator", column = "C_CREATOR"),
+            @Result(property = "createTime", column = "C_CREATETIME"),
+            @Result(property = "modifier", column = "C_MODIFIER"),
+            @Result(property = "modifyTime", column = "C_MODIFYTIME"),
+            @Result(property = "state", column = "c_state")
+    })
+    Sale findSaleContract(@Param("id") long id);
 
     @Select("select count(c_id) from t_sale where c_customer_id = #{customerId}")
     long countSalesByCustomer(@Param("customerId")long id);
@@ -28,7 +42,7 @@ public interface SaleMapper {
             " AND s.c_target_type = #{condition.targetType}",
             "</if>",
             "</script>"})
-    long countSaleContracts(SaleContractQueryVo condition);
+    long countSaleContracts(@Param("condition")SaleContractQueryVo condition);
 
     @Select({"<script>",
             "SELECT s.* FROM t_sale_contract s",
@@ -37,10 +51,10 @@ public interface SaleMapper {
             " AND s.c_code = #{condition.code}",
             "</if>",
             "<if test='condition.targetType != null'>",
-            " AND m.c_target_type = #{condition.targetType}",
+            " AND m.c_target_type = #{condition.type}",
             "</if>",
             " ORDER By s.c_createtime desc ",
-            " LIMIT ?,?",
+            " LIMIT #{offset},#{pageSize}",
             "</script>"})
-    ArrayList<Sale> pageSaleContracts(SaleContractQueryVo condition, int offset, int pageSize);
+    ArrayList<Sale> pageSaleContracts(@Param("condition")SaleContractQueryVo condition, @Param("offset")int offset, @Param("pageSize")int pageSize);
 }
